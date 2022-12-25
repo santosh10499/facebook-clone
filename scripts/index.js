ba58804b7f3dfac56844980e3a85743175be8041
santosh10499 <santoshkumarmehtaqwerty@gmail.com>
2022-12-25 20:39:14 +0530

    api update

--- scripts/index.js
+++ scripts/index.js
@@ -1,202 +1,10 @@
-let data = [
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "varsha",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "/images/post2.png",
-    like: 17,
-    comment: 48,
-    share: "",
-  },
-
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "Nikita",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "https://scontent.fdel15-1.fna.fbcdn.net/v/t39.30808-6/319366497_856641655537554_6911095490872985966_n.jpg?stp=dst-jpg_s960x960&_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=xStYXxEiZW4AX_4YZFE&_nc_ht=scontent.fdel15-1.fna&oh=00_AfCvCIvC_6AC7T8FJa1C190SIy22sGBFn_lMHSYqJIPXbQ&oe=63A95744",
-    like: 17,
-    comment: 48,
-    share: "",
-  },
-
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "Aditya",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "/images/friend5.png",
-    like: 171,
-    comment: 87,
-    share: "",
-  },
-
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "Robin",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "https://scontent.fdel15-1.fna.fbcdn.net/v/t39.30808-6/319366497_856641655537554_6911095490872985966_n.jpg?stp=dst-jpg_s960x960&_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=xStYXxEiZW4AX_4YZFE&_nc_ht=scontent.fdel15-1.fna&oh=00_AfCvCIvC_6AC7T8FJa1C190SIy22sGBFn_lMHSYqJIPXbQ&oe=63A95744",
-    like: 147,
-    comment: 78,
-    share: "",
-  },
-
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "Ashutosh",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "/images/friend6.png",
-    like: 17,
-    comment: 487,
-    share: "",
-  },
-
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "varsha",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "/images/post1.png",
-    like: 17,
-    comment: 48,
-    share: "",
-  },
-
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "RJ",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "https://scontent.fdel15-1.fna.fbcdn.net/v/t39.30808-6/320786628_885398515819748_6194837900968737595_n.jpg?stp=dst-jpg_s960x960&_nc_cat=106&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=lAvzgTIUB8UAX_2_3fo&tn=uVVaWKvuBVBVoPQW&_nc_ht=scontent.fdel15-1.fna&oh=00_AfD6a0nm-sxj_fhtk2zHHtqR7NnZ9JhVBGeFzE9cj3WktQ&oe=63A90D6E",
-    like: 184,
-    comment:47,
-    share: "",
-  },
-  
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "Nikita",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "https://scontent.fdel15-1.fna.fbcdn.net/v/t39.30808-6/319366497_856641655537554_6911095490872985966_n.jpg?stp=dst-jpg_s960x960&_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=xStYXxEiZW4AX_4YZFE&_nc_ht=scontent.fdel15-1.fna&oh=00_AfCvCIvC_6AC7T8FJa1C190SIy22sGBFn_lMHSYqJIPXbQ&oe=63A95744",
-    like: 789,
-    comment: 478,
-    share: "",
-  },
-
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "varsha",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "/images/post2.png",
-    like: 17,
-    comment: 48,
-    share: "",
-  },
-
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "Nikita",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "https://scontent.fdel15-1.fna.fbcdn.net/v/t39.30808-6/319366497_856641655537554_6911095490872985966_n.jpg?stp=dst-jpg_s960x960&_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=xStYXxEiZW4AX_4YZFE&_nc_ht=scontent.fdel15-1.fna&oh=00_AfCvCIvC_6AC7T8FJa1C190SIy22sGBFn_lMHSYqJIPXbQ&oe=63A95744",
-    like: 17,
-    comment: 15,
-    share: "",
-  },
-
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "Aditya",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "/images/friend5.png",
-    like: 178,
-    comment:184,
-    share: "",
-  },
-
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "Robin",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "https://scontent.fdel15-1.fna.fbcdn.net/v/t39.30808-6/319366497_856641655537554_6911095490872985966_n.jpg?stp=dst-jpg_s960x960&_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=xStYXxEiZW4AX_4YZFE&_nc_ht=scontent.fdel15-1.fna&oh=00_AfCvCIvC_6AC7T8FJa1C190SIy22sGBFn_lMHSYqJIPXbQ&oe=63A95744",
-    like: 185,
-    comment: 100,
-    share: "",
-  },
-
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "varsha",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "/images/post2.png",
-    like: 187,
-    comment: 40,
-    share: "",
-  },
-
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "Nikita",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "https://scontent.fdel15-1.fna.fbcdn.net/v/t39.30808-6/319366497_856641655537554_6911095490872985966_n.jpg?stp=dst-jpg_s960x960&_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=xStYXxEiZW4AX_4YZFE&_nc_ht=scontent.fdel15-1.fna&oh=00_AfCvCIvC_6AC7T8FJa1C190SIy22sGBFn_lMHSYqJIPXbQ&oe=63A95744",
-    like: 107,
-    comment: 48,
-    share: "",
-  },
-
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "Aditya",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "/images/friend5.png",
-    like: 148,
-    comment: 478,
-    share: "",
-  },
-
-  {
-    userImage:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-oxeye-daisy-flower_181624-11106.jpg?auto=format&h=200",
-    userName: "Robin",
-    time: "20h",
-    caption: "lLorem ipsum dolor sit amet consectetur adipisicing elit.",
-    post: "https://scontent.fdel15-1.fna.fbcdn.net/v/t39.30808-6/319366497_856641655537554_6911095490872985966_n.jpg?stp=dst-jpg_s960x960&_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=xStYXxEiZW4AX_4YZFE&_nc_ht=scontent.fdel15-1.fna&oh=00_AfCvCIvC_6AC7T8FJa1C190SIy22sGBFn_lMHSYqJIPXbQ&oe=63A95744",
-    like: 170,
-    comment: "",
-    share: "",
-  },
-];
-
 const dummy_post = document.querySelector(".dummy_post");
+async function api() {
+  let data1 = await fetch("https://63a7491a59fd83b1bb42ba9a.mockapi.io/api/v1/facbook_posts");
+  let data = await data1.json();
 
-const dataRender = () => {
   for (let i = 0; i < data.length; i++) {
-     dummy_post.innerHTML += `<div class="dummy_post">
+    dummy_post.innerHTML += `<div class="dummy_post">
       <div class="top">
         <img src="${data[i].userImage}" alt="" class="profile bg hover" width="45px" />
         <div class="name">
@@ -225,18 +33,32 @@ const dataRender = () => {
         </div>
       </div>
       <div class="reaction">
-        <span class="like"><p class="hover "><i class="fa-regular icon  fa-thumbs-up"></i> Like</p></span>
+        <span class="like1"><p class="hover "><i class="fa-regular icon  fa-thumbs-up"></i> Like</p></span>
         <p class="hover"><i class="fa-regular fa-message"></i> Comment</p>
         <p class="hover"><i class="fa-solid fa-share"></i> Share</p>
       </div>
     </div>
 `;
-// dummy_post.insertAdjacentHTML("afterbegin", post)
+    // dummy_post.insertAdjacentHTML("afterbegin", post)
   }
-};
-// window.addEventListener("load", dataRender)
-dataRender()
+  // ====================this is for likeing post
 
+  let likebtn = document.querySelectorAll(".like1");
+  let icon = document.querySelectorAll(".icon");
+  let likeCount = document.querySelectorAll(".like-count");
+
+  for (let j = 0; j < likebtn.length; j++) {
+    likebtn[j].addEventListener("click", () => {
+      likeCount[j].innerHTML = `<span style="margin-left:-20px" class="pointer like-count" > ${data[j].like + 1}</span>`;
+
+      likebtn[j].style.color = "blue";
+      icon[j].style.color = "blue";
+    });
+  }
+}
+api();
+
+// window.addEventListener("load", dataRender)
 
 // pop up on photo/video
 
@@ -252,48 +74,43 @@ Add_post.addEventListener("click", (e) => {
   }
 });
 
-
 // click on fa-xmark
 
-const fa_xmark  = document.querySelector(".fa-xmark")
+const fa_xmark = document.querySelector(".fa-xmark");
 
-fa_xmark.addEventListener("click", ()=>{
-  if (photo_video.style.display == "block" ) {
+fa_xmark.addEventListener("click", () => {
+  if (photo_video.style.display == "block") {
     photo_video.style.display = "none";
   }
+});
 
-})
-
-// create_new_group 
+// create_new_group
 
 const create_new_group_popUp = document.querySelector("#create_new_group");
 const plus_btn = document.querySelector("#plus_btn");
 const New_message = document.querySelector("#New_message");
 
-plus_btn.addEventListener("click", ()=>{
+plus_btn.addEventListener("click", () => {
   if (create_new_group_popUp.style.display == "block") {
     create_new_group_popUp.style.display = "none";
   } else {
     create_new_group_popUp.style.display = "block";
   }
-})
+});
 
-New_message.addEventListener("click", ()=>{
-  if (create_new_group_popUp.style.display == "block" ) {
+New_message.addEventListener("click", () => {
+  if (create_new_group_popUp.style.display == "block") {
     create_new_group_popUp.style.display = "none";
   }
-
-})
-
+});
 
 // for profile page
 
-const profile_page = document.querySelector("#profile_page")
+const profile_page = document.querySelector("#profile_page");
 
 profile_page.addEventListener("click", () => {
   window.location.href = "/pages/profile.html";
 });
-
 
 // fb_logo to home page
 
@@ -314,52 +131,36 @@ home.addEventListener("click", () => {
 // console.log(home.innerHTML)
 const friendIcon = document.querySelector("#friend");
 
-
 friendIcon.addEventListener("click", () => {
   window.location.href = "/pages/friends.html";
 });
 
-let likebtn=document.querySelectorAll('.like');
-let icon=document.querySelectorAll('.icon');
-let likeCount=document.querySelectorAll('.like-count');
-console.log(likebtn)
-
-
-for(let j=0;j<likebtn.length;j++)
-{
-  likebtn[j].addEventListener('click',()=>{
-    
-      likeCount[j].innerHTML=`<span style="margin-left:-20px" class="pointer like-count" > ${data[j].like+1}</span>`;
-     
-     likebtn[j].style.color="blue";
-     icon[j].style.color="blue";
-     
-    
-})
-}
-
-
-
 //=========================for displaying user profile=============
 
-let userdata=[{email: "santosh@gmail.com",profilePic: "/images/santosh-mehta-profilepic.jpg"},
-             {email: "varsharajput353@gmail.com",profilePic: "/images/varsha-profilepic.jpg"},
-             {email:"ss0149952@gmail.com",profilePic: "/images/sonu-profilepic.jpg"},
-             {email: "rohanpalankar8@gmail.com",profilePic: "/images/profile-pic-rohan.jpg"}];
+let userdata = [
+  {
+    email: "santosh@gmail.com",
+    profilePic: "/images/santosh-mehta-profilepic.jpg",
+  },
+  {
+    email: "varsharajput353@gmail.com",
+    profilePic: "/images/varsha-profilepic.jpg",
+  },
+  { email: "ss0149952@gmail.com", profilePic: "/images/sonu-profilepic.jpg" },
+  {
+    email: "rohanpalankar8@gmail.com",
+    profilePic: "/images/profile-pic-rohan.jpg",
+  },
+];
 
-  let Email=localStorage.getItem('userEmail');
-  console.log(Email)
-let userimage=document.querySelectorAll('.userimage');
+let Email = localStorage.getItem("userEmail");
+console.log(Email);
+let userimage = document.querySelectorAll(".userimage");
 
-for(let k=0;k<userdata.length;k++)
-{
-for(let i=0;i<userimage.length;i++)
-{
-   if(userdata[k].email==Email)
-   {
-    
-   userimage[i].src=userdata[k].profilePic
-   }
+for (let k = 0; k < userdata.length; k++) {
+  for (let i = 0; i < userimage.length; i++) {
+    if (userdata[k].email == Email) {
+      userimage[i].src = userdata[k].profilePic;
+    }
+  }
 }
-}
-
