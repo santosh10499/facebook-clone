b674c61c6da7cd03795a52ff9b9650f8c8199d69
Rohan Palankar <rohanpalankar8@gmail.com>
2022-12-26 02:26:03 +0530

    modify files

--- scripts/login.js
+++ scripts/login.js
@@ -14,12 +14,46 @@ let array = JSON.parse(data);
 console.log(array)
 //===================================click event on login form matching data==========
 
-login.addEventListener("click", () => {
+// login.addEventListener("click", () => {
   
-  for (let i = 0; i<array.length; i++) {
-    if (array[i].email == email.value && array[i].password == password.value) {
+//   for (let i = 0; i<array.length; i++) {
+
+//     if (array[i].email == email.value && array[i].password == password.value) {
        
-      // this is used for render name in home page
+//       // this is used for render name in home page
+//        userEmail.push(array[i].email)
+//        firstName.push(array[i].userName);
+//        lastName.push(array[i].surName);
+//        localStorage.setItem('userEmail',userEmail);
+//        localStorage.setItem('firstName',firstName);
+//        localStorage.setItem('LastName',lastName)
+
+//       //  ---------------------------------
+      
+//       email.style.border = "1px solid black";
+//       password.style.border = "1px solid black";
+//       window.location.href ="/pages/header.html";
+      
+//     } else {
+//       alert("Wrong email id")
+//       email.style.border = "1px solid red";
+//       password.style.border = "1px solid red";
+//     }
+    
+//   }
+// });
+
+
+
+login.addEventListener("click", () => {
+  let wrongEmail = true;
+  for (let i = 0; i<array.length; i++) {
+
+    if (array[i].email == email.value){
+      wrongEmail = false;
+      if (array[i].password == password.value){
+
+        // this is used for render name in home page
        userEmail.push(array[i].email)
        firstName.push(array[i].userName);
        lastName.push(array[i].surName);
@@ -32,17 +66,26 @@ login.addEventListener("click", () => {
       email.style.border = "1px solid black";
       password.style.border = "1px solid black";
       window.location.href ="/pages/header.html";
-      
-    } else {
-      
-      email.style.border = "1px solid red";
-      password.style.border = "1px solid red";
+
+      }
+      else {
+        password.style.border = "1px solid red";
+        alert("Wrong Password"); 
+      }
     }
-    
   }
 
-   
+  if (wrongEmail){
+    email.style.border = "1px solid red";
+    alert("User doesn't exist");
+  }
+
 });
+
+
+
+
+
 
 // ================================click event on click button]]]]]]]]]]]]]]]]]]]]]]]
 createAccount.addEventListener("click", () => {
